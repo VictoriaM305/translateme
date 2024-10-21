7fb7e25301b53e6dbc2d506ceae02a585f1b732a
Victoria Mckinnie <Vmcki004@fiu.edu>
2024-10-20 23:47:42 -0400

    Initial Commit

---
+++ translateme/translatemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  translatemeApp.swift
+//  translateme
+//
+//  Created by user on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct translatemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
